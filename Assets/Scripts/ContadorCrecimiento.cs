2bb8adae3da0646bd2df9d85281f03f43763ec61
guseppe <yucepi@gmail.com>
2021-10-11 22:40:26 -0400

    Corrección de bug al activar experiencia 2

--- Assets/Scripts/ContadorCrecimiento.cs
+++ Assets/Scripts/ContadorCrecimiento.cs
@@ -11,25 +11,33 @@ public class ContadorCrecimiento : MonoBehaviour
 	public	int tiempoInicial;
 	
 	[Tooltip("Escala del tiempo del reloj")]
-	public	float	EscalaDeTiempo = 12;
+	public	float	EscalaDeTiempo = 12, EscalaDeTiempoCierreCompletado = 60;
 	
 	public	TMP_Text MyText;
 	private	float TiempoDelFrameConTimeScale = 0f;
-	private	float TiempoAMostrarEnDias = 0f;
+	private	float TiempoAMostrarEnDias = 0f, tiempo_cierre = 0f;
 	private float EscalaDeTiempoAlPausar, EscalaDeTiempoInicial;
 	private	bool EstaPausado = false;
+	private int segundos_cierre = 0;
 	
 	public GameObject Panel1, Panel2, Panel3, Panel4, AlertaCompletado;
 	
-
+	// This function is called when the object becomes enabled and active.
+	protected void OnEnable()
+	{
+		EscalaDeTiempoInicial = EscalaDeTiempo;
+		MyText = GetComponent<TMP_Text>();
+		TiempoAMostrarEnDias = tiempoInicial;	
+		ActualizarReloj(tiempoInicial);
+	}
     // Start is called before the first frame update
-    void Start()
-    {
-	    EscalaDeTiempoInicial = EscalaDeTiempo;
-	    MyText = GetComponent<TMP_Text>();
-	    TiempoAMostrarEnDias = tiempoInicial;	
-	    ActualizarReloj(tiempoInicial); 
-    }
+    //void Start()
+    //{
+	//    EscalaDeTiempoInicial = EscalaDeTiempo;
+	//    MyText = GetComponent<TMP_Text>();
+	//    TiempoAMostrarEnDias = tiempoInicial;	
+	//    ActualizarReloj(tiempoInicial); 
+    //}
 
     // Update is called once per frame
     void Update()
@@ -54,7 +62,6 @@ public class ContadorCrecimiento : MonoBehaviour
 		//calcular minutos y segundos
 		semanas = (int)tiempoensegundos/30;
 		dias = (int)tiempoensegundos%30;
-		
 		//crear la cadena de caracteres con 2 digitos para los minutos y segundos,separados por ":"
 		TextoReloj =( semanas.ToString("00") + " Semanas\n"+ dias.ToString(" 00")+" Días");
 		
@@ -72,14 +79,31 @@ public class ContadorCrecimiento : MonoBehaviour
 				activar_panel(Panel3);
 					break;
 			case 04:
+				if(EstaPausado == false)
 				activar_panel(Panel4);
 					break;
 		}
 
-		if(semanas == 04){
-			pausar();
-			Panel4.SetActive(false);
-			AlertaCompletado.SetActive(true);
+		if(semanas == 04 && EstaPausado == false){
+			if(AlertaCompletado.active == false)
+			{
+				pausar();
+				Panel4.SetActive(false);
+				AlertaCompletado.SetActive(true);
+			}
+			
+		}
+		if(EscalaDeTiempo == 0)
+		{
+			if(segundos_cierre < 5)
+			{
+				tiempo_cierre += Time.deltaTime*EscalaDeTiempoCierreCompletado;
+				segundos_cierre = (int)tiempo_cierre/60;
+				if(segundos_cierre == 4)
+				{
+					AlertaCompletado.SetActive(false);
+				}
+			}
 		}
 	}
 	
@@ -93,17 +117,29 @@ public class ContadorCrecimiento : MonoBehaviour
 	
 	private void activar_panel(GameObject panel_activado)
 	{
+		desactivar_todos();
+		panel_activado.SetActive(true);
+	}
+	
+	private void desactivar_todos()
+	{
 		Panel1.SetActive(false);
 		Panel2.SetActive(false);
 		Panel3.SetActive(false);
 		Panel4.SetActive(false);
-		panel_activado.SetActive(true);
 	}
-	
 	// This function is called when the behaviour becomes disabled () or inactive.
 	protected void OnDisable()
 	{
 		AlertaCompletado.SetActive(false);
+		desactivar_todos();
+		EscalaDeTiempo = 12;
+		EscalaDeTiempoCierreCompletado = 60;
+		tiempo_cierre = 0f;
+		TiempoAMostrarEnDias = 0f;
+		TiempoDelFrameConTimeScale = 0f;
+		segundos_cierre = 0;
+		EstaPausado = false;
 	}
 	
 }
