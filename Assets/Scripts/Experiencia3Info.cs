11264afaf8492113eb967a74ba3fb3aa3e306ac0
guseppe <yucepi@gmail.com>
2021-10-13 14:59:41 -0400

    Se posicionaron los objetos UI contenidos en el canva 3D de la experiencia 3, dentro del modelo 3D
    Se creó un script para la selección de la información a mostrar en la experiencia 3

---
+++ Assets/Scripts/Experiencia3Info.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experiencia3Info : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] infos;
+	public void AlternarInfo(string info_destino)
+	{
+		foreach (var info in infos)
+		{
+			if(info.name == info_destino)
+			{
+			   if(!info.active)
+	           {
+						      info.SetActive(true);
+			   }
+			   else
+			   {
+						      info.SetActive(false);
+					   }
+			}
+			else
+			{
+			  	info.SetActive(false);
+			}
+		}
+	}
+}
