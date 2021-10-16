00421e88dca13373d4e6889fb13571abfac5ea42
Jhilario-dev <31388439+Jhilario-dev@users.noreply.github.com>
2021-10-16 13:13:54 -0400

    feat: compartir fotos

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -10,14 +10,9 @@ using UnityEngine.Android;
 public class Camara : MonoBehaviour
 {
 	public GameObject[] ObjetosAOcultar;
-	public GameObject panelEscaner;
-	public GameObject panelFoto;
-	public GameObject objetoImagen;
-	public GameObject AdvertenciaGuardar;
-	public GameObject AdvertenciaDescartar;
+	public GameObject[] ocultarObjCamara;
 	
-	Texture2D lastImageTexture;
-	string lastFileName = "";
+	string filePath;
 	
 	
 	// Start is called before the first frame update
@@ -34,99 +29,60 @@ public class Camara : MonoBehaviour
 		}
 	}
     
-	public void EncenderUnPanel(GameObject panel){
-		panelEscaner.SetActive(false);
-		panelFoto.SetActive(false);
-		panel.SetActive(true);
-		
-		if(panel == panelFoto){
-			UpdateImage();
-		}
-	}
-	
-	void UpdateImage(){
-		lastImageTexture = GetScreenshotImage();
-		Sprite sp = Sprite.Create(lastImageTexture, new Rect(0,0, lastImageTexture.width, lastImageTexture.height), new Vector2(0.5f, 0.5f));
-		objetoImagen.GetComponent<Image>().sprite = sp;
-	}
-	
-	Texture2D GetScreenshotImage(){
-		Texture2D texture = null;
-		byte[] fileBytes;
-		fileBytes = File.ReadAllBytes(Application.persistentDataPath+"/"+lastFileName);
-		texture = new Texture2D(2,2, TextureFormat.RGB24, false);
-		texture.LoadImage(fileBytes);
-		return texture;
-	}
-	
-	void Update(){
-		if(File.Exists(Application.persistentDataPath+"/"+lastFileName)){
-			SetActivesObjetos(true);
-			EncenderUnPanel(panelFoto);
-		}
-	}
-	
     
-	void SetActivesObjetos(bool valor){
+	public void SetActivesObjetos(bool valor){
 		for(int i = 0; i<ObjetosAOcultar.Length; i++){
 			ObjetosAOcultar[i].SetActive(valor);
 		}
 	}
-
+	
+	
+	public void SetObjetosCamara(bool valor){
+		for(int i = 0; i<ObjetosAOcultar.Length; i++){
+			ocultarObjCamara[i].SetActive(valor);
+		}
+	}
+	
+	
 	public void TakeScreenShot(){
+		SetActivesObjetos(false);
 		StartCoroutine("CaptureIt");
 	}
 	
 	IEnumerator CaptureIt(){
 		
-		SetActivesObjetos(false);
-		string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
-		string fileName = "MuralAR-" + timeStamp + ".png";
-		lastFileName = fileName;
-		ScreenCapture.CaptureScreenshot(fileName);
-
+		
 		yield return new WaitForEndOfFrame();
+		Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+		ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+		ss.Apply();
+		filePath = Path.Combine( Application.temporaryCachePath, "shared img.png" );
+		File.WriteAllBytes( filePath, ss.EncodeToPNG() );
+
+		
+		// To avoid memory leaks
+		Destroy( ss );
+
+		SetActivesObjetos(true);
+		
 	}
 	
 	public void Descartar(){
-		File.Delete(Application.persistentDataPath+"/"+lastFileName);
+
 		SetActivesObjetos(true);
-		AdvertenciaDescartar.SetActive(false);
-		EncenderUnPanel(panelEscaner);
 	}
-	
 	
 	public void Guardar(){
 		NativeGallery.SaveImageToGallery();
 
-		File.WriteAllBytes( "/storage/emulated/0/Mural/tabaco.png",lastImageTexture.EncodeToPNG());
 	}
-	
-	void OnImageSaved(string loquesea){
-	}
-	
-	void DespuesDeGuardar(bool sucess, string path){
-		if(sucess){
-			AdvertenciaGuardar.SetActive(true);
-			Descartar();
-		}
-	}
-	
-	public void DesactivarAdvertencia(){
-		AdvertenciaDescartar.SetActive(false);
-		AdvertenciaGuardar.SetActive(false);
-	}
-	
 	
 	public void Compartir(){
-		new NativeShare().AddFile( lastImageTexture, lastFileName)
+		
+		new NativeShare().AddFile( filePath )
 			.SetSubject( "MuralAR" ).SetText( "Mural Photo" )
 			.SetCallback( ( result, shareTarget ) => Debug.Log( "Share result: " + result + ", selected app: " + shareTarget ) )
 			.Share();
-	}
-	
-	public void volverApaneles(){
-		SceneManager.LoadScene("paneles");
 	}
 	
 }
